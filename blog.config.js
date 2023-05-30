bbf0fa30679b4bbcbc50a846dee8120c1fda5593
忙巳 <109103173+lveMonsi@users.noreply.github.com>
2023-05-30 21:24:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'lveMonsi的Test',
+  title: 'lveMonsiのBlog',
   author: 'lveMonsi',
-  email: 'i@craigary.net',
+  email: 'lvemonsi@gmail.com',
   link: 'https://lveMonsi.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: 'NO GAME NO LIFE',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
@@ -20,7 +20,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/craigaryhart',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Blog', 'Website', 'Notion', 'Python','C++','Windows','Linux','PyQt','html','css'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
